94fcdbe006d14b91b4760546f9265768cd725d58
zeglor <zeglor@gmail.com>
2015-11-01 04:43:28 +0300

    replaced tabs with whitespaces in db.py

--- db.py
+++ db.py
@@ -24,82 +24,85 @@
 
 from uuid import uuid4
 
+
 class Db:
-	_instance = None
-	
-	def __init__(self):
-		# initialize connections
-		pass
-	
-	def generateKey(self):
-		pass
-	
-	def store(self, key, objSerial):
-		pass
-	
-	def retrieve(self, key):
-		pass
-	
-	def keyExists(self, key):
-		pass
-	
-	@classmethod
-	def instance(cls):
-		if cls._instance is None:
-			cls._instance = cls()
-		return cls._instance
+    _instance = None
+
+    def __init__(self):
+        # initialize connections
+        pass
+
+    def generateKey(self):
+        pass
+
+    def store(self, key, objSerial):
+        pass
+
+    def retrieve(self, key):
+        pass
+
+    def keyExists(self, key):
+        pass
+
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
+
 
 class DbTest(Db):
-	_instance = None
-	storage = {}
-	lists = {}
-	
-	def __init__(self):
-		super().__init__()
-	
-	def generateKey(self):
-		return str(uuid4())
-	
-	def store(self, key, objSerial):
-		DbTest.storage[key] = objSerial
-	
-	def retrieve(self, key):
-		return DbTest.storage.get(key)
-	
-	def lenList(self, name):
-		try:
-			return len(self.lists[name])
-		except KeyError:
-			return 0
-	
-	def listAppend(self, name, val):
-		if self.lists.get(name) is None:
-			self.lists[name] = []
-		self.lists[name].append(val)
-	
-	def listPopLeft(self, name):
-		if self.lists.get(name) is None or len(self.lists[name]) == 0:
-			return None
-		return self.lists[name].pop(0)
-	
-	def retrieveList(self, name):
-		if self.lists.get(name) is None:
-			return None
-		return self.lists[name]
-	
-	def removeFromList(self, name, item):
-		self.lists[name].remove(item)
-	
-	def keyExists(self, key):
-		return key in DbTest.storage
+    _instance = None
+    storage = {}
+    lists = {}
+
+    def __init__(self):
+        super().__init__()
+
+    def generateKey(self):
+        return str(uuid4())
+
+    def store(self, key, objSerial):
+        DbTest.storage[key] = objSerial
+
+    def retrieve(self, key):
+        return DbTest.storage.get(key)
+
+    def lenList(self, name):
+        try:
+            return len(self.lists[name])
+        except KeyError:
+            return 0
+
+    def listAppend(self, name, val):
+        if self.lists.get(name) is None:
+            self.lists[name] = []
+        self.lists[name].append(val)
+
+    def listPopLeft(self, name):
+        if self.lists.get(name) is None or len(self.lists[name]) == 0:
+            return None
+        return self.lists[name].pop(0)
+
+    def retrieveList(self, name):
+        if self.lists.get(name) is None:
+            return None
+        return self.lists[name]
+
+    def removeFromList(self, name, item):
+        self.lists[name].remove(item)
+
+    def keyExists(self, key):
+        return key in DbTest.storage
+
 
 def main():
-	db = DbTest.instance()
-	objSerial = "test"
-	key = db.generateKey()
-	db.store(key, objSerial)
-	print(db.retrieve(key))
+    db = DbTest.instance()
+    objSerial = "test"
+    key = db.generateKey()
+    db.store(key, objSerial)
+    print(db.retrieve(key))
+
 
 if __name__ == '__main__':
-	main()
-
+    main()
